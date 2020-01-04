bf79dec2b7ba5b21dd89ac03a7d48f7616ea3eff
Oshonika <Oshonika@LAPTOP-59QOCOB2>
2020-01-04 23:04:59 +0530

    I want to commit all the project

---
+++ Project_3/src/Pkg3/Assignment1.java
@@ -0,0 +1,54 @@
+package Pkg3;
+
+public class Assignment1 {
+
+
+public int sum(int a,int b)
+{
+int sum;
+sum=a+b; 
+System.out.println("Sum = " + sum);
+return sum;
+}
+public int sub(int c,int d)
+  {
+   int sub;
+	sub=c-d;
+	System.out.println("Sub = " + sub);
+	return sub;
+  }
+ public int mul(int e,int f)
+ {
+ int mul;
+ mul=e*f;
+ System.out.println("mul = " + mul);
+ return mul;
+ }
+ public void div(int g,int h)
+ {
+	 int div;
+	 div=g/h;
+	 System.out.println("Final Result of ((((10+2)-2)+2)*2)/2) = " + div);	
+ }
+ public static void main(String[] args)
+ {
+	 Assignment1 obj=new Assignment1();
+	 int sum=obj.sum(10,2);
+	 int sub=obj.sub(sum,2);
+	 int sum1=obj.sum(sub,2);
+	 int mul=obj.mul(sum1,2);
+	 obj.div(mul, 2);
+ } 
+	 
+}	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	
+			
+
+ 
